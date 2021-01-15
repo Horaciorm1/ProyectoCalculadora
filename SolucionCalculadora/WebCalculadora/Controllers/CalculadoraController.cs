775f5d79afdea12a2cb266c21237c2b073e08f0d
Horacio Rodrigo Morales Cruz <a2018119192@estudiantes.upsa.edu.bo>
2021-01-14 20:25:53 -0400

    Suma, Resta con GET y POST

--- SolucionCalculadora/WebCalculadora/Controllers/CalculadoraController.cs
+++ SolucionCalculadora/WebCalculadora/Controllers/CalculadoraController.cs
@@ -11,16 +11,62 @@ namespace WebCalculadora.Controllers
     public class CalculadoraController : Controller
     {
        [HttpGet]
+       [Route("suma")]
         public int suma(int num1, int num2)
         {
             return num1 + num2;
         }
         [HttpPost]
+        [Route("suma")]
         public int sumar([FromHeader] int num1, [FromHeader] int num2)
         {
             return num1 + num2;
         }
 
-      
+
+        [HttpGet]
+        [Route("resta")]
+        public int resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        
+        [HttpPost]
+        [Route("resta")]
+        public int restar([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 - num2;
+        }
+
+       /* [HttpGet]
+        [Route("multi")]
+        public int multi(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        
+        [HttpPost]
+        [Route("multi")]
+        public int multix([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 * num2;
+        }
+
+        [HttpGet]
+        [Route("div")]
+        public int div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+        [HttpPost]
+        [Route("div")]
+        public int divs([FromHeader] int num1, [FromHeader] int num2)
+        {
+            return num1 / num2;
+        }*/
     }
 }
